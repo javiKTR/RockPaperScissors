ba0485614439e3ec850c19edbc9651abcf712cfb
javiKTR <javieraznarcau@gmail.com>
2023-04-07 18:44:42 +0200

    Elección usuario y maquina y victoria

--- scritp.js
+++ scritp.js
@@ -2,3 +2,29 @@
 function startGame() {
     window.location.href = "RockPaperScissors.html"
 }
+
+var userSelection
+var computerSelection
+
+// función que obtiene la selección del usuario
+
+function getUserSelection(n) {
+    userSelection = n
+
+    // dar un valor alearotio a computerSelection
+    computerSelection = Math.floor(Math.random() * 3)
+    getResult()
+}
+
+// función que redirecciona a la pagina de resultado
+function getResult() {
+    window.location.href = "result.html"
+    document.write(userSelection + " - " + computerSelection)
+    if (((userSelection - 1 ) % 3) == computerSelection){
+        document.write("Victory")
+    }else if (userSelection == computerSelection){
+        document.write("Tie")
+    }else{
+        document.write("Defeat")
+    }
+}
